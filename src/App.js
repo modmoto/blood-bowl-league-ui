18023b9d3cd6ebf74ea3ecc673071b07cc1c9c2e
Simon Heiss <simonheiss87@gmail.com>
2019-10-31 18:54:05 +0100

    added style and color

--- src/App.js
+++ src/App.js
@@ -11,38 +11,51 @@ import {
 import LeaguePage from "./League/LeaguePage";
 import MyTeamPage from "./MyTeam/MyTeamPage";
 import UpcomingGamePage from "./UpcominGames/UpcomingGamesPage";
+import Home from "./Home/HomePage";
+
+import { createMuiTheme } from '@material-ui/core/styles';
+import {ThemeProvider} from "@material-ui/styles";
+
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: '#81c784',
+        },
+        secondary: {
+            main: '#ffcc80',
+        },
+    },
+});
 
 function App() {
-
     return (
         <Router>
-            <AppBar position="static">
-                <Toolbar>
-                    <Button  component={Link} to={'/league'} color="inherit">League</Button>
-                    <Button  component={Link} to={'/upcoming-games'} color="inherit">Upcoming Games</Button>
-                    <Button  component={Link} to={'/my-team'} color="inherit">My Team</Button>
-                </Toolbar>
-            </AppBar>
+            <ThemeProvider theme={theme}>
+                <AppBar position="static">
+                    <Toolbar>
+                        <Button  component={Link} to={'/'} color="inherit">Kabbl</Button>
+                        <Button  component={Link} to={'/league'} color="inherit">League</Button>
+                        <Button  component={Link} to={'/upcoming-games'} color="inherit">Upcoming Games</Button>
+                        <Button  component={Link} to={'/my-team'} color="inherit">My Team</Button>
+                    </Toolbar>
+                </AppBar>
 
-            <Switch>
-                <Route path="/league">
-                    <LeaguePage />
-                </Route>
-                <Route path="/upcoming-games">
-                    <UpcomingGamePage />
-                </Route>
-                <Route path="/my-team">
-                    <MyTeamPage />
-                </Route>
-                <Route path="/">
-                    <Home />
-                </Route>
-            </Switch>
+                <Switch>
+                    <Route path="/league">
+                        <LeaguePage />
+                    </Route>
+                    <Route path="/upcoming-games">
+                        <UpcomingGamePage />
+                    </Route>
+                    <Route path="/my-team">
+                        <MyTeamPage />
+                    </Route>
+                    <Route path="/">
+                        <Home />
+                    </Route>
+                </Switch>
+            </ThemeProvider>
         </Router>
     )}
-
-function Home() {
-    return <h2>Home</h2>;
-}
 
 export default App;
---
+++ src/Home/HomePage.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+    icon: {
+        marginRight: theme.spacing(2),
+    },
+    heroContent: {
+        backgroundColor: theme.palette.background.paper,
+        padding: theme.spacing(10, 0, 6),
+    }
+}));
+
+function Home() {
+    const classes = useStyles();
+
+    return (
+    <Container maxWidth="sm" className={classes.heroContent}>
+        <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
+            Karlsruher Blood Bowl Liga
+        </Typography>
+        <Typography variant="h5" align="center" color="textSecondary" paragraph>
+            in und um Karlsruhe seit  2008
+        </Typography>
+    </Container>
+    )
+}
+
+export default Home;
