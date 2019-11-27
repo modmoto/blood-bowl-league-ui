abc01baa6a9d5f7fd8be7d5117497b324e7ed2aa
Simon Heiss <simonheiss87@gmail.com>
2019-11-28 00:20:46 +0100

    refactored

--- src/GlobalErrorStateReducer.js
+++ src/GlobalErrorStateReducer.js
@@ -2,21 +2,21 @@ export default function globalErrorStateReducer(state = {
     errorOccured: false,
     message: null
 }, action) {
+
+    function actionObject(action) {
+        return {
+            ...state,
+            errorOccured: true,
+            message: action.message,
+            result: action.result
+        };
+    }
+
     switch (action.type) {
         case 'BACKEND_GET_CALL_FAILED':
-            return {
-                ...state,
-                errorOccured: true,
-                message: action.message,
-                result: action.result
-            };
+            return actionObject(action);
         case 'BACKEND_POST_CALL_FAILED':
-            return {
-                ...state,
-                errorOccured: true,
-                message: action.message,
-                result: action.result
-            };
+            return actionObject(action);
 
         default:
             return state
