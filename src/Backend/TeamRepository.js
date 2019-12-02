9b92028c4a488d049f9a82191df7faf8001d6487
Simon Heiss <simonheiss87@gmail.com>
2019-12-02 07:55:21 +0100

    fixed backendcall

--- src/Backend/TeamRepository.js
+++ src/Backend/TeamRepository.js
@@ -1,9 +1,9 @@
 import {fetchJson, sendJson} from "./RepositoryBase";
 
-//const ReadURl = 'https://teams-readhost.herokuapp.com/api'
-//const WriteUrl = 'https://teams-host.herokuapp.com/api'
-const ReadURl = 'http://localhost:5000/api'
-const WriteUrl = 'http://localhost:5001/api'
+const ReadURl = 'https://teams-readhost.herokuapp.com/api';
+const WriteUrl = 'https://teams-host.herokuapp.com/api';
+//const ReadURl = 'http://localhost:5000/api'
+//const WriteUrl = 'http://localhost:5001/api'
 
 export async function fetchTeamCall(teamId) {
     return await fetchJson(ReadURl, "/Teams/" + teamId + "/full")
