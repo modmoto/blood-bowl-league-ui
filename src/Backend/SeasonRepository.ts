ab79e0544260291b576fd4838624e89d08da455e
Simon Heiss <simonheiss87@gmail.com>
2019-12-02 18:06:55 +0100

    added first typescript

--- src/Backend/SeasonRepository.ts
+++ src/Backend/SeasonRepository.ts
@@ -1,9 +1,10 @@
 import {fetchJson} from "./RepositoryBase";
+import {GameDay} from "../Models/GameDay";
 
-const BackendUrl = 'https://seasons-readhost.herokuapp.com/api'
+const BackendUrl = 'https://seasons-readhost.herokuapp.com/api';
 //const BackendUrl = 'http://localhost:5006/api'
 
-export async function fetchUpcomingGames(seasonId) {
+export async function fetchUpcomingGames(seasonId: string) : Promise<GameDay[]> {
     let path = "/Seasons/" + seasonId + "/gameDays";
     return await fetchJson(BackendUrl, path)
 }
---
+++ src/Models/GameDay.ts
@@ -0,0 +1,4 @@
+export interface GameDay {
+    HomeTeamName: string;
+    GuestTeamName: string;
+}
