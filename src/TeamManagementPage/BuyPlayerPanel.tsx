f066cd383ecd889b6ed530ea016664b2c248899d
Simon Heiss <simonheiss87@gmail.com>
2019-12-04 00:02:58 +0100

    added default value to buy player stuff

--- src/TeamManagementPage/BuyPlayerPanel.tsx
+++ src/TeamManagementPage/BuyPlayerPanel.tsx
@@ -27,7 +27,7 @@ const BuyPlayerPanel:FunctionComponent<{
     const allowedPlayers = fullTeam.allowedPlayers;
     const raceOfPlayer = races.filter(r => r.raceConfigId === fullTeam.raceId)[0];
     const startingPlayer = raceOfPlayer ? raceOfPlayer.allowedPlayers[0].playerTypeId : '';
-    const realValue = (selectedPlayerType === '') ? startingPlayer : selectedPlayerType;
+    const realValue = (selectedPlayerType !== '') ? selectedPlayerType : startingPlayer;
 
     const onBuyPlayerClick = (type: string) => {
         const playerConfig = raceOfPlayer.allowedPlayers.filter(p => p.playerTypeId === type)[0];
