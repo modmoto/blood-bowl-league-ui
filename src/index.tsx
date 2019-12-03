cb6ba146922ecf9cafac71106e459c89f569607d
Simon Heiss <simonheiss87@gmail.com>
2019-12-03 09:25:43 +0100

    deleted last css file

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 
 import i18n from "i18next";
 import {initReactI18next} from "react-i18next";
 import languages from "./i18nResources/languages.js";
-//import {getBrowserLanguage} from "./helpers";
+import {getBrowserLanguage} from "./helpers";
 
-//const language = getBrowserLanguage()
+const language = getBrowserLanguage();
 
 i18n
     .use(initReactI18next)
     .init({
         resources: languages,
-        lng: 'en',
+        lng: language,
         fallbackLng: "de",
     });
 
