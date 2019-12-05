c393af02cf9b6f5c7926ae2f7565d096990d4f4d
Simon Heiss <simonheiss87@gmail.com>
2019-12-05 08:23:31 +0100

    Revert "repo now typesafe"
    This reverts commit ebfaec02

--- src/Backend/SeasonRepository.ts
+++ src/Backend/SeasonRepository.ts
@@ -1,14 +1,10 @@
 import {fetchJson} from "./RepositoryBase";
 import {GameDay} from "../Models/Seasons/GameDay";
-import {Matchup} from "../Models/Seasons/Matchup";
 
 const BackendUrl = 'https://seasons-readhost.herokuapp.com/api';
 //const BackendUrl = 'http://localhost:5006/api'
 
 export async function fetchUpcomingGames(seasonId: string): Promise<GameDay[]> {
     let path = "/Seasons/" + seasonId + "/gameDays";
-    const rawGameDays = await fetchJson(BackendUrl, path);
-
-    // @ts-ignore
-    return rawGameDays.map(g => new GameDay(g.matchups.map(m => new Matchup(m.homeTeamName, m.guestTeamName))));
+    return await fetchJson(BackendUrl, path)
 }
