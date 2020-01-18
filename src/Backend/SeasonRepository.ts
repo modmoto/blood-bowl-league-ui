6e31931056a804f6724afdda659c83cb96a77f67
Simon Heiss <simonheiss87@gmail.com>
2020-01-18 03:29:25 +0100

    fix to 404

--- src/Backend/SeasonRepository.ts
+++ src/Backend/SeasonRepository.ts
@@ -1,8 +1,7 @@
 import {fetchJson} from "./RepositoryBase";
 import {GameDay} from "../Models/Seasons/GameDay";
 
-//const BackendUrl = 'http://localhost:5001/api';
-const BackendUrl = 'http://h2865571.stratoserver.net/:5001/api';
+const BackendUrl = 'http://h2865571.stratoserver.net:5001/api';
 
 export async function fetchUpcomingGames(seasonId: string): Promise<GameDay[]> {
     let path = "/Seasons/" + seasonId + "/gameDays";
